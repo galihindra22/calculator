63c3a9fbcda0deec07ed1cfc66b266cc318e26a6
galihindra22 <galihindranevano@gmail.com>
2026-07-20 14:43:45 +0700

    Add buttons and operate function

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+let num1;
+let num2;
+let operation;
+
+function operate(num1, operation, num2){
+    //if operation equals to something call that function something
+    
+}
+
 function add(num1, num2){
     return num1+num2;
 }
