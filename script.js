0e4d922b5d5afac373f63eaa45324422c26f9b95
galihindra22 <galihindranevano@gmail.com>
2026-07-21 13:46:52 +0700

    Make the calculator work
    Get the value from buttons and then pass it to operate, the operate
    function will determine to choose from the 4 operations. It returns
    the result to the display. For now it's working but I shall see
    with edge cases if it's still working.

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
-
 let num1;
 let num2;
-let operation;
+let operator;
+let resetDisplay = false;
+let res;
 
 const buttons = document.querySelectorAll("button");
-console.log(buttons);
 buttons.forEach(element => {
     const value = element.textContent;
     element.addEventListener("click", () => display(value));
-
-       
 });
 
-
-function display(value){
+function display(values){
     const display = document.getElementById("display");
-    if(value === "C"){
+       
+    if(values === "C"){
         display.textContent = "";
     }
+    else if(values === "+" || values === "-" || values === "/" || values === "*"){
+        num1 = display.textContent;
+        operator = values;
+        console.log(operator);
+        console.log(num1);
+        resetDisplay = true;
+    }
+    else if(values === "="){
+        if(num1 !== undefined){
+            num2 = display.textContent;
+            console.log(num2);
+        }
+
+    }
     else{
-        display.textContent += value;
+        if(resetDisplay === true){
+            display.textContent = "";
+            resetDisplay = false;
+        }
+        display.textContent += values;
+    }
+
+    if(num1 !== undefined && num2 !== undefined && operator !== undefined){
+        display.textContent = operate(num1, operator, num2);
     }
     
 }
 
 function operate(num1, operation, num2){
     //if operation equals to something call that function something
-    
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch(operation){
+        case "+": 
+            res = add(num1, num2);
+            break;
+        case "-":
+            res = substract(num1, num2);
+        case "*":
+            res = multiply(num1, num2);
+        case "/":
+            res = divide(num1, num2);
+    }
+    return res;
 }
 
 function add(num1, num2){
