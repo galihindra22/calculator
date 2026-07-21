44e140e00dd7eb1e875ddafdaf201ff3b1f09796
galihindra22 <galihindranevano@gmail.com>
2026-07-21 12:02:59 +0700

    Display value when buttons are clicked

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ let num1;
 let num2;
 let operation;
 
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+buttons.forEach(element => {
+    const value = element.textContent;
+    element.addEventListener("click", () => display(value));
+
+       
+});
+
+
+function display(value){
+    const display = document.getElementById("display");
+    if(value === "C"){
+        display.textContent = "";
+    }
+    else{
+        display.textContent += value;
+    }
+    
+}
+
 function operate(num1, operation, num2){
     //if operation equals to something call that function something
     
